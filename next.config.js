5e5f14a942d52acd5549f09225de9c0e9a1a7504
Kamil <kamil.dmytrenko@gmail.com>
2022-06-05 16:16:48 +0200

    env changes

--- next.config.js
+++ next.config.js
@@ -14,12 +14,7 @@ const nextConfig = {
       '/kontakt': { page: '/kontakt' },
     }
   },
-  env: {
-    STATIC_FORMS_URL: "https://api.staticforms.xyz/submit",
-    STATIC_FORMS_ACCESS_KEY: "9e972397-26d8-408d-a85b-287651d5402a",
-    CONTENTFUL_SPACE_ID: "4xmsmjmdyvkq",
-    CONTENTFUL_ACCESS_TOKEN: "6NRcPg3NLYwNxPZ3T46RMQEdVq1KuiHeHeXC0SOVcN4"
-  },
+  env: {},
   images: {
     loader: "custom",
     domains:['images.ctfassets.net'],
